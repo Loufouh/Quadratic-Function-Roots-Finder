249d3ff3f9ed95d591e5a92f3822408f4469fdf1
Louis700 <33942287+Louis700@users.noreply.github.com>
2018-10-01 18:47:11 +0200

    Add comment for Math.map()

--- inc/math/Math.js
+++ inc/math/Math.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// To re-maps a value from a range (inMin inMax) to another (outMin outMax)
 Math.map = function (x, inMin, inMax, outMin, outMax) {
 	return (x - inMin)*(outMax - outMin)/(inMax - inMin) + outMin;
 } 
